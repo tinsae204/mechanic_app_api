ca18e09793d73b283c80484dc94695705a38d8ef
Tinsae D Yirgu <tinsudessalegn204@gmail.com>
2022-05-17 09:49:27 +0300

    user accounts migrated to postgress db

---
+++ app_api/accounts/models.py
@@ -0,0 +1,34 @@
+from multiprocessing.reduction import AbstractReducer
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_trmanager = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+    is_mechanic = models.BooleanField(default=False)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+class CustomAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class TAmanager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    fullname = models.CharField(max_length=100)
+    phoneno = models.CharField(max_length=15)
+
+class Mechanic(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    phoneno = models.CharField(max_length=15)
+    specialization = models.CharField(max_length=100)
+    education = models.CharField(max_length=100)
+    docs = models.FileField(upload_to='uploads', blank=True)
+    is_online = models.BooleanField(default=False)
+    is_suspended = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- app_api/app_api/settings.py
+++ app_api/app_api/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Application definition
 
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'app_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mechanic_app_db',
+        'USER': 'postgres',
+        'PASSWORD': '204tinsae',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
